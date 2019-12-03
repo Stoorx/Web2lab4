2b7062adf88614bce5cd2c59a7b9a6929a909a15
Stoorx <antiproton2013@yandex.ru>
2019-12-03 21:50:31 +0300

    Add favoritesRepo

---
+++ favoritesRepo.js
@@ -0,0 +1,54 @@
+import mysql from "mysql";
+import config from "./config";
+
+const connection = () => mysql.createConnection({
+    host: config.database.host,
+    user: config.database.user,
+    database: config.database.database,
+    password: config.database.password
+});
+
+export const getCities = () => new Promise((resolve, reject) => {
+    const c = connection();
+    c.connect((err) => {
+        if (err)
+            reject(err);
+
+        c.query("SELECT name FROM `fav_cities`", (err, record) => {
+            c.end();
+            if (err)
+                reject(err);
+            resolve(record);
+        })
+    })
+});
+
+export const addCity = (cityName) => new Promise((resolve, reject) => {
+    const c = connection();
+    c.connect((err) => {
+        if (err)
+            reject(err);
+
+        c.query("INSERT INTO `fav_cities` SET name = ?", [cityName], (err, record) => {
+            c.end();
+            if (err)
+                reject(err);
+            resolve(record);
+        })
+    })
+});
+
+export const deleteCity = (cityName) => new Promise((resolve, reject) => {
+    const c = connection();
+    c.connect((err) => {
+        if (err)
+            reject(err);
+
+        c.query("DELETE FROM `fav_cities` WHERE name = ?", [cityName], (err, record) => {
+            c.end();
+            if (err)
+                reject(err);
+            resolve(record);
+        })
+    })
+});
