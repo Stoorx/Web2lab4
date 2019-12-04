0b0581296d40e0b63f75a6e14a2cf583a6c580e6
Stoorx <antiproton2013@yandex.ru>
2019-12-04 09:28:01 +0300

    Add favorites endpoints

--- routes/favorites.js
+++ routes/favorites.js
@@ -1,7 +1,31 @@
 import express from "express";
+import * as repo from "../favoritesRepo"
 
 export default () =>
     express.Router()
-        .get('/', (req, res) =>
-            res.send('OK')
+        .get('/', async (req, res) => {
+                try {
+                    res.json(await repo.getCities());
+                } catch (e) {
+                    res.status(400).json({error: e})
+                }
+            }
+        )
+        .post('/', async (req, res) => {
+                try {
+                    await repo.addCity(req.body.name);
+                    res.json({result: "OK"});
+                } catch (e) {
+                    res.status(400).json({error: e})
+                }
+            }
+        )
+        .delete('/', async (req, res) => {
+                try {
+                    await repo.deleteCity(req.body.name);
+                    res.json({result: "OK"});
+                } catch (e) {
+                    res.status(400).json({error: e})
+                }
+            }
         );
